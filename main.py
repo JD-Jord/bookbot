6ab358f38e832f4afd2d72b0bec4b7205e9eba97
JD-Jord <jordan_oshea@hotmail.co.uk>
2024-11-29 11:21:09 +0000

    Added character count book report

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ def main():
     text = read_text(book_path)
     num_words = get_numbers(text)
     characters = char_appearances(text)
-    print(f"{num_words} words found in the document")
-    print(characters)
-
+    characters_list = dict_to_list(characters)
+    characters_list.sort(reverse=True, key=dict_sort)
+    print(f"--- Begin report of {book_path} --- \n{num_words} words found in the document")
+    for d in characters_list:
+        for k,v in d.items():
+            print(f"The '{k}' character was found {v} times")
 def read_text(path):
     with open(path) as f:
         return f.read()
@@ -20,6 +23,17 @@ def char_appearances(text):
     for c in set(text):
         dict[c] = text.count(c)
     return dict
+
+def dict_sort(dict):
+    return list(dict.values())[0]
+
+def dict_to_list(dict):
+    dict_list = []
+    for key, value in dict.items():
+        if key.isalpha() == True:
+            new_dict = {key: value}
+            dict_list.append(new_dict)
+    return dict_list
 
 
 main()
